de0415a96bba40d38f6b98e37ce7dbe07211e00f
Collin Stedman <cstedman@princeton>
2013-12-19 17:32:19 -0600

    Added passive updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Express */
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 /* Request */
 var request = require('request');
@@ -8,26 +9,40 @@ var request = require('request');
 /* Credentials */
 var credentials = require('./credentials');
 
+/* Socket.io */
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 var countdown = 0;
+var update = false;
 
 setInterval(function() {
-  countdown -= 10000;
-  if (countdown < 0) countdown = 0;
-  request('https://api.spark.io/v1/devices/' + credentials.id + 
-          '/detected?access_token=' + credentials.token, function(error, resp, body) {
-    if (!error && resp.statusCode == 200) {
-      if (JSON.parse(body).result[3] == 1) {
-        countdown = 300000;
+  countdown -= 3000;
+  if (countdown <= 0) {
+    countdown = 0;
+    request('https://api.spark.io/v1/devices/' + credentials.id + 
+            '/detected?access_token=' + credentials.token, function(error, resp, body) {
+      // Add logic here
+      if (!error && resp.statusCode == 200) {
+        if (JSON.parse(body).result[3] == 1) {
+          countdown = 10000;
+          io.sockets.emit('response', { data: 'Intruder detected!'});
+          update = true;
+        }
+        else if (update) {
+          io.sockets.emit('response', { data: 'All quiet'});
+          update = false;
+        }
       }
-    }
-  });
-}, 10000);
-
-app.get('/', function(req, res) {
-  if (countdown > 0) {
-    res.send("Intruder detected!");
+    });
   }
-  else res.send("All quiet");
+}, 3000);
+
+io.sockets.on('connection', function(socket) {
+  if (countdown > 0) {
+    socket.emit('response', { data: 'Intruder detected!'});
+  }
+  else socket.emit('response', { data: 'All quiet'});
 });
 
-app.listen(7000);
+server.listen(7000);
